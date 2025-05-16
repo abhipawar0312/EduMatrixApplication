52d5706802fcec655b04792ed4e67f9cfe21fa56
Abhishek Pawar <98691564+abhipawar0312@users.noreply.github.com>
2025-05-17 00:26:58 +0530

    First Spring Boot Commit

---
+++ src/main/java/com/example/EduMatrix/EduMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduMatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduMatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EduMatrix/EduMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EduMatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
